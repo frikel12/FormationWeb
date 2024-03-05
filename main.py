f1bb8d53c61557e2c2e2e2695058480790cb903b
Frikel Youssef <yousseffrikel7@gmail.com>
2024-03-05 19:46:15 +0100

    add login and register

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ def index():
     return render_template('base.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 db_config = {
     'host': 'localhost',
     'user': 'root',
